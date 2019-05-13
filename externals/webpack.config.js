afa4e9f216718f7e38aa84756c3b585874bf5227
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-13 13:15:49 -0400

    Utilizando path para acceder a las rutas relativas

--- externals/webpack.config.js
+++ externals/webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: path.resolve(__dirname, 'index.js'),
   output: {
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
   }
 }
+
+// path me permite obtener rutas relativas
+// path.resolve(__dirname, 'index.js') => Significa que va a buscar dentro del directorio en el que
+// estoy, el archivo llamado 'index.js'
