fa753952bca173f123cd289611aba3ecdd952578
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-13 13:47:12 -0400

    Copiando carpeta de css-style-loader para hacer practica con extract-text

---
+++ plugin-extract-text/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // Aquí van los loaders
+      {
+        // test: que tipo de archivo quiero reconocer
+        test: /\.css$/,
+        // use: que loader se va a encargar del archivo
+        use: [
+          'style-loader',
+          'css-loader'
+        ],
+      }
+    ]
+  }
+}
