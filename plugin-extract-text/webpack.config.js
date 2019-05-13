32852b81245b45f812c4f30ff4d94365aa4a2f42
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-13 14:00:01 -0400

    Importando nuevos estilos en el index.html mediante el tag link

--- plugin-extract-text/webpack.config.js
+++ plugin-extract-text/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -14,11 +15,16 @@ module.exports = {
         // test: que tipo de archivo quiero reconocer
         test: /\.css$/,
         // use: que loader se va a encargar del archivo
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
+        use: ExtractTextPlugin.extract({
+          // ['style-loader','css-loader']
+          use: 'css-loader',
+          // fallback: 'style-loader'
+        }),
       }
     ]
-  }
+  },
+  plugins: [
+    //Aqui van los plugins
+    new ExtractTextPlugin("css/styles.css")
+  ]
 }
