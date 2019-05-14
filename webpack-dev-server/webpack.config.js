6c81d076c93c0822439b555595309d98a547a7e4
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-14 14:42:07 -0400

    ignore

--- webpack-dev-server/webpack.config.js
+++ webpack-dev-server/webpack.config.js
@@ -3,9 +3,6 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: path.resolve(__dirname, 'index.js'),
-  // devServer: {
-  //   contentBase: path.join(__dirname, 'dist')
-  // },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
@@ -17,11 +14,8 @@ module.exports = {
         // test: que tipo de archivo quiero reconocer
         test: /\.css$/,
         // use: que loader se va a encargar del archivo
-        use: [
-          'style-loader',
-          'css-loader'
-        ],
+        use: ['style-loader','css-loader'],
       }
     ]
-  },
+  }
 }
