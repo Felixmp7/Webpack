8efa59fd7f250e8630c6830b577cc8946bbd8335
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-14 15:19:21 -0400

    creando carpeta, babel-loader

---
+++ babel-loader/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      // Aquí van los loaders
+      {
+        // test: que tipo de archivo quiero reconocer
+        test: /\.css$/,
+        // use: que loader se va a encargar del archivo
+        use: ExtractTextPlugin.extract({
+          // ['style-loader','css-loader']
+          use: 'css-loader',
+          // fallback: 'style-loader'
+        }),
+      }
+    ]
+  },
+  plugins: [
+    //Aqui van los plugins
+    new ExtractTextPlugin("css/[name].css")
+  ]
+}
