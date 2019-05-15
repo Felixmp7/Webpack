f1e83134cdd18c88c6781404a155340160b89031
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-15 09:26:09 -0400

    Creando nueva carpeta para el url-loader de videos

---
+++ url-loader-video/src/js/index.js
@@ -0,0 +1,14 @@
+import '../css/estilos.css'
+import { firstMessage, delayedMessage } from './message.js'
+import banner from '../images/banner1.jpg'
+
+const img = document.createElement('img')
+img.setAttribute('src', banner)
+img.setAttribute('width', 100)
+img.setAttribute('height', 50)
+
+// console.log('Hola mundo desde Webpack');
+console.log('Hola mundo desde Webpack en webpack.config.js');
+document.write(firstMessage)
+document.body.append(img)
+delayedMessage();
