2113e513bdc2219e3756442971b8fdc2122567e7
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-15 14:45:23 -0400

    Creando carpeta para practicar con JSON

---
+++ json/src/js/message.js
@@ -0,0 +1,16 @@
+import renderToDOM from './render-to-dom.js'
+import makeMessage from './make-message'
+
+const waitTime = new Promise((resolve,reject) => {
+  setTimeout( () => {
+    resolve('han pasado 3 segundos')}, 3000);
+})
+
+export const firstMessage = 'Hola mundo desde un modulo'
+export const delayedMessage = async () => {
+    const message = await waitTime;
+    console.log(message);
+    // const element = document.createElement('p');
+    // element.textContent = message;
+    renderToDOM(makeMessage(message));
+  }
