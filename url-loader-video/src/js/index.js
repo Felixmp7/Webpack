1af9c6ef3e8c3766f88c43114b2baadeb3a81705
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-15 09:39:53 -0400

    Importando video para renderizarlo

--- url-loader-video/src/js/index.js
+++ url-loader-video/src/js/index.js
@@ -1,14 +1,20 @@
 import '../css/estilos.css'
 import { firstMessage, delayedMessage } from './message.js'
 import banner from '../images/banner1.jpg'
+import platzi from '../videos/que-es-core.mp4'
 
 const img = document.createElement('img')
 img.setAttribute('src', banner)
 img.setAttribute('width', 100)
 img.setAttribute('height', 50)
 
-// console.log('Hola mundo desde Webpack');
-console.log('Hola mundo desde Webpack en webpack.config.js');
+const video = document.createElement('video')
+video.setAttribute('src', platzi)
+video.setAttribute('width', 480)
+video.setAttribute('autoplay', true)
+video.setAttribute('controls', true)
+
 document.write(firstMessage)
 document.body.append(img)
 delayedMessage();
+document.body.append(video)
