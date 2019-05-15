cf4d21bc03537ebd79f7870077f013c36280d826
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-15 09:56:02 -0400

    Estableciendo el publicPath

--- url-loader-video/webpack.config.js
+++ url-loader-video/webpack.config.js
@@ -6,7 +6,8 @@ module.exports = {
   entry: path.resolve(__dirname, 'src/js/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    publicPath: './dist/'
   },
   module: {
     rules: [
