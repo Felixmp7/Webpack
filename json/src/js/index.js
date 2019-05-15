c80621a2856f1b8833fce2142fe1452418db3159
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-15 14:56:25 -0400

    Compilando y probando console.log de la data

--- json/src/js/index.js
+++ json/src/js/index.js
@@ -1,14 +1,17 @@
 import '../css/estilos.css'
 import { firstMessage, delayedMessage } from './message.js'
 import banner from '../images/banner1.jpg'
+import data from './teachers.json'
+
+console.log(data);
+
+document.write(firstMessage)
+delayedMessage();
 
 const img = document.createElement('img')
 img.setAttribute('src', banner)
 img.setAttribute('width', 100)
 img.setAttribute('height', 50)
-
+document.body.append(img)
 // console.log('Hola mundo desde Webpack');
 console.log('Hola mundo desde Webpack en webpack.config.js');
-document.write(firstMessage)
-document.body.append(img)
-delayedMessage();
