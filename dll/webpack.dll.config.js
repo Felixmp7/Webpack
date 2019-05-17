266665088a48e585d03ca1b4875d717a93f1053e
Felixmp_a <felixmanuelpachecoalonso@gmail.com>
2019-05-17 17:20:48 -0400

    Creando plugin de Dll

--- dll/webpack.dll.config.js
+++ dll/webpack.dll.config.js
@@ -8,10 +8,6 @@ module.exports = {
     vendor: [
       'react',
       'react-dom',
-      //jquery,
-      //bootstrap,
-      //immutable,
-      //etc.
     ],
     home: path.resolve(__dirname, 'src/js/index.js'),
     contact: path.resolve(__dirname, 'src/js/contact.js')
@@ -20,52 +16,56 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js'
   },
-  module: {
-    rules: [
-      // Aquí van los loaders
-      {
-        // test: que tipo de archivo quiero reconocer
-        test: /\.js$/,
-        // use: que loader se va a encargar del archivo
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [["@babel/preset-env",{ "targets": { "node": "current" } }], '@babel/react']
-          }
-        }
-      },
-      {
-        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 150000
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: 'css-loader',
-        }),
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: ExtractTextPlugin.extract({
-          use: ['css-loader','sass-loader']
-        }),
-      }
-    ]
-  },
+ // module: {
+ //    rules: [
+ //      // Aquí van los loaders
+ //      {
+ //        // test: que tipo de archivo quiero reconocer
+ //        test: /\.js$/,
+ //        // use: que loader se va a encargar del archivo
+ //        use: {
+ //          loader: 'babel-loader',
+ //          options: {
+ //            presets: [["@babel/preset-env",{ "targets": { "node": "current" } }], '@babel/react']
+ //          }
+ //        }
+ //      },
+ //      {
+ //        test: /\.(jpg|png|gif|woff|eot|ttf|svg)$/,
+ //        use: {
+ //          loader: 'url-loader',
+ //          options: {
+ //            limit: 150000
+ //          }
+ //        }
+ //      },
+ //      {
+ //        test: /\.css$/,
+ //        use: ExtractTextPlugin.extract({
+ //          use: 'css-loader',
+ //        }),
+ //      },
+ //      {
+ //        test: /\.(sass|scss)$/,
+ //        use: ExtractTextPlugin.extract({
+ //          use: ['css-loader','sass-loader']
+ //        }),
+ //      }
+ //    ]
+ //  },
   plugins: [
     //Aqui van los plugins
-    new ExtractTextPlugin("css/[name].css")
+    //new ExtractTextPlugin("css/[name].css")
+    new webpack.DllPlugin({
+      name:'[name]',
+      path: path.join(__dirname, '[name]-manifest.json')
+    })
   ],
-  optimization: {
-    splitChunks: {
-      minChunks: 2,
-      name: "vendor",
-      chunks: "initial"
-    }
-  }
+  // optimization: {
+  //   splitChunks: {
+  //     minChunks: 2,
+  //     name: "vendor",
+  //     chunks: "initial"
+  //   }
+  // }
 }
